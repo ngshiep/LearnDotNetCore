a6f3f7fb1c10f193d4e849b6d1a1ecb5281a72b9
hiepNguyen <nshiepnuce@gmail.com>
2022-06-11 11:21:15 +0700

    Create Application library

--- CoreData/Entities/Lop.cs
+++ CoreData/Entities/Lop.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -8,6 +9,7 @@ namespace Core.Data.Entities
     [Table("Lop")]
     public class Lop
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }        
     }
